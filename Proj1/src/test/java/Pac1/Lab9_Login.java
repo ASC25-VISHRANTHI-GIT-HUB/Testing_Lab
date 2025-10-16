ec0c837fa77652864cb24a1721e35019519a3615
vishranthi.a <vishranthi.a@192.168.2.72>
2025-10-16 15:44:50 +0530

    lab completed

---
+++ Proj1/src/test/java/Pac1/Lab9_Login.java
@@ -0,0 +1,21 @@
+package Pac1;
+
+public class Lab9_Login {
+    // Credentials
+    private String username = "username123";
+    private String password = "pass123";
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    // Setters (if needed for flexibility)
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
